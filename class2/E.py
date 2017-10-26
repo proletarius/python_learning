3e47bf3bbb47eef4d3816ee4bfb00c4a4f7ee0dd
proletarius <s.kulchanovskiy@gmail.com>
2017-10-26 17:32:23 +0600

    Task E added second variant

--- class2/E.py
+++ class2/E.py
@@ -1,10 +1,15 @@
 # Задача E. Максимум двух чисел
 # http://informatics.mccme.ru/mod/statements/view3.php?id=16205&chapterid=3501#1
 
-a = int(input("Введите число 1: "))
-b = int(input("Введите число 2: "))
+# вариант первый
+#a = int(input("Введите число 1: "))
+#b = int(input("Введите число 2: "))
 
-if a >= b:
-    print(a)
-else:
-    print(b)
+#if a >= b:
+#    print(a)
+#else:
+#    print(b)
+
+# вариант второй
+a,b = int(input()),int(input())
+print(a if a >= b else b)
