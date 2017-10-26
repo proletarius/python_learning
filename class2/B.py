b6abf62cb6914d35709bc02d35750a1eadaa21dc
proletarius <s.kulchanovskiy@gmail.com>
2017-10-26 17:08:18 +0600

    Task B added

---
+++ class2/B.py
@@ -0,0 +1,12 @@
+# Задача B. Факториал
+# http://informatics.mccme.ru/mod/statements/view3.php?id=16205&chapterid=3444#1
+
+# вариант первый
+#n = 1
+#for i in range(2,21,1):
+#    n *= i
+#print(n)
+
+# вариант второй
+from math import factorial
+print(factorial(20))
